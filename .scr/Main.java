d053eae96acb81eeee37149380d7894a3d3b565f
Henrinoeldner <henri.noeldner@frg.wwschool.de>
2026-01-10 21:04:20 +0100

    Interface funktioniert

--- .scr/Main.java
+++ .scr/Main.java
@@ -19,13 +19,30 @@ public class Main {
          ShopClient kunde = new ShopClient("localhost",16);
 
          while(true) {
-             System.out.println("Was möchtest du machen? \n [1] Neues TShirt bestellen \n [2] Bestellung bestätigung \n [3]abmelden");
+             System.out.println("Was möchtest du machen? \n [1] Neues TShirt bestellen \n [2] Bestellung bestätigung \n [3] Abmelden");
              int auswahl = scanner.nextInt();
              if (auswahl==1){
-
+                 String groessse;
+                 String farbe;
+                 System.out.println("In welcher Größe möchtest du dein TShirt bestellen? (S/M/L/XL/XXl)");
+                 groessse = scanner.next();
+                 System.out.println("Welche Farbe soll das TShirt haben? ");
+                 farbe=scanner.next();
+                 kunde.groesseFarbeWaehlen(groessse,farbe);
+             }else if( auswahl==2){
+                 System.out.println("Ja oder Nein");
+                 String antwort= scanner.next();
+                 kunde.bestaetigen(antwort);
+             }else if (auswahl == 3){
+                 kunde.abmelden();
+             }else {
+                 System.out.println("Ungültige eingabe. Bitte Nutzte nur 1,2 oder 3");
              }
+             /*
              kunde.groesseFarbeWaehlen("s", "Grün");
              kunde.bestaetigen("Ja");
+
+              */
          }
          //Connection connection=new Connection("localhost",16);
          //DaytimeClient daytimeClient= new DaytimeClient("time.fu-berlin.de");
